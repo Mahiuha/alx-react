d5366e7642ef296f82ca758214847d66a8eefbe5
Mahiuha <tiasho.mahiuha@gmail.com>
2023-02-23 19:28:29 +0300

    basic setup

---
+++ task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from "jquery";
+
+const $ = require( "jquery" );
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
